be20164c831f8b2a0c4b07947ed1e96b0eb015e1
arila22 <arila.behrami@student.uni-pr.edu>
2024-12-29 19:05:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
 let search=document.querySelector('.search-box');
 
 
-document.querySelector('#search-icon').onclick = () =>{
+document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
-    navbar.classList.remove('active');
+    cart.classList.remove('active');
+    user.classList.remove('active');
 }
 
 
-let navbar=document.querySelector('.navbar');
+let cart=document.querySelector('.cart');
 
-
-document.querySelector('#menu-icon').onclick = () =>{
-    navbar.classList.toggle('active');
+document.querySelector('#cart-icon').onclick = () => {
+    cart.classList.toggle('active');
     search.classList.remove('active');
+    user.classList.remove('active');
 }
 
-window.onscroll = () =>{
-    navbar.classList.remove('active');
+let user=document.querySelector('.user');
+
+
+document.querySelector('#user-icon').onclick = () => {
+    user.classList.toggle('active');
     search.classList.remove('active');
+    cart.classList.remove('active');
 }
 
-let header=document.querySelector('header');
 
+document.getElementById('checkout').addEventListener('click', function() {
+    // Merrni të dhënat nga formulari
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var address = document.getElementById('address').value;
+    var product = document.getElementById('product').value;
+    var payment = document.getElementById('payment').value;
+    var total = document.getElementById('total').textContent;
 
-window.addEventListener('scroll', () => {
-    header.classList.toggle('shadow',window.scrollY > 0);
+    // Dërgoni këto të dhëna në elementët përmbledhës
+    document.getElementById('summary-name').textContent = "Emri: " + name;
+    document.getElementById('summary-email').textContent = "Email: " + email;
+    document.getElementById('summary-address').textContent = "Adresa: " + address;
+    document.getElementById('summary-product').textContent = "Produkt: " + product;
+    document.getElementById('summary-payment').textContent = "Pagesa: " + payment;
+    document.getElementById('summary-total').textContent = "Totali: " + total;
+
+    // Shfaqni seksionin e përmbledhjes
+    document.getElementById('order-summary').style.display = 'block';
 });
+
+
