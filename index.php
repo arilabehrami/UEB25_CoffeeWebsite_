e71700c05814484dd7c1ddd1e631f575bb19142d
agnesasuhodolli <agnesa.suhodolli@student.uni-pr.edu>
2025-04-17 18:57:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -118,7 +118,7 @@
     <section class="home" id="home">
         <div class="home-text">
            <h1> <i>Start your day<br>with coffee</i></h1>
-            <a href="products.html" class="btn" >Shop Now</a>
+            <a href="products.php" class="btn" >Shop Now</a>
         </div>
         <div class="home-video">
             <video  autoplay loop muted src="images/coffee.mp4"></video>
