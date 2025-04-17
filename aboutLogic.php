f09b422955e62833e34a8986de8c24cbaaf04c74
agnesasuhodolli <agnesa.suhodolli@student.uni-pr.edu>
2025-04-17 20:31:45 +0200

    Update aboutLogic.php

--- aboutLogic.php
+++ aboutLogic.php
@@ -1,7 +1,7 @@
 <?php
 //Definimi i konstantes per emrin e kafenese dhe daten e themelimit
-define("COFFEE_SHOP_NAME", "Delight")
-define("FOUNDED_YEAR", 2020);
+define("coffeeShopName", "Delight");
+define("foundedYear", "2020");
 
 //Variablat globale
 $misioniKafenese = "To provide the best coffee experience to our customers.";
@@ -10,12 +10,12 @@ $lokacionet = [
     "Prishtine" => ["Adresa" => "Agim Ramadani Str", "Nr. Tel" => "+38349552378"],
     "Mitrovice" => ["Adresa" => "Shemsi Ahmeti Str", "Nr. Tel" => "+38344778535"],
     "Peje" => ["Adresa" => "Enver Hadri Str", "Nr. Tel" => "+38349522332"]
-]
+];
 
 //Funksioni qe shfaq te dhenat e kompanise (vitin e themelimit, si dhe nje mesazh mireseardhje)
 function shfaqCompInfo() {
-    echo "<h1>Welcome to " . COFFEE_SHOP_NAME . "! Here's all you need to know about us.</h1>";
-    echo "<p>Founded in " . FOUNDED_YEAR . ", our mission is: " . $misioniKafenese . ".</p>"
+    echo "<h1>Welcome to " . coffeeShopName . "! Here's all you need to know about us.</h1>";
+    echo "<p>Founded in " . foundedYear . ", our mission is: " . $GLOBALS['misioniKafenese'] . ".</p>";
 }
 
 //Funksioni qe shfaq team members
@@ -38,7 +38,7 @@ function shfaqLokacionet($locations) {
 }
 
 //Kushtezimet qe kontrollojne nese kafeneja eshte nje kompani e re
-if(date("Y") - FOUNDED_YEAR < 5) {
+if(date("Y") - foundedYear < 5) {
     echo "<p>We are a new Coffee Shop, but we are passionate about all types of coffee!</p>";
 } else {
     echo "<p>With years of experience, we know how to serve the best coffee to our customers!</p>";
