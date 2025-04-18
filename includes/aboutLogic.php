095d2ce0564626c3ed1533ae81576ffa4f813924
arila22 <arila.behrami@student.uni-pr.edu>
2025-04-18 23:29:32 +0200

    Update aboutLogic.php

--- includes/aboutLogic.php
+++ includes/aboutLogic.php
@@ -14,10 +14,9 @@ $lokacionet = [
 
 //Funksioni qe shfaq te dhenat e kompanise (vitin e themelimit, si dhe nje mesazh mireseardhje)
 function shfaqCompInfo() {
-    echo "<h1>Welcome to " . coffeeShopName . "! Here's all you need to know about us.</h1>";
-    echo "<p>Founded in " . foundedYear . ", our mission is: " . $GLOBALS['misioniKafenese'] . "</p>";
+    echo "<h1  style='margin: 20px; font-weight: bold; text-align: center; color: white;>Welcome to " . coffeeShopName . "! Here's all you need to know about us.</h1>";
+    echo "<p >Founded in " . foundedYear . ", our mission is: " . $GLOBALS['misioniKafenese'] . "</p>";
 }
-
 //Funksioni qe shfaq team members
 function shfaqTeamMembers($members) {
     echo "<h2>Meet Our Team</h2><ul>";
